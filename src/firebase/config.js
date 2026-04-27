0318891d2b3805bebfd92d88b8e3877d071b2980
Prof. Luiz Camilo <luiz.camilo4@etec.sp.gov.br>
2026-04-27 19:01:39 -0300

    teste

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,6 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getDatabase } from 'firebase/database';
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
   // apiKey: 'SUA_API_KEY',
@@ -11,6 +11,13 @@ const firebaseConfig = {
   // messagingSenderId: 'SEU_MESSAGING_SENDER_ID',
   // appId: 'SEU_APP_ID',
 
+  apiKey: "AIzaSyDgt92xf3s19QPz3sSEvlui-Lr_sC5UVYU",
+  authDomain: "fiap-aulas-14d7a.firebaseapp.com",
+  databaseURL: "https://fiap-aulas-14d7a-default-rtdb.firebaseio.com",
+  projectId: "fiap-aulas-14d7a",
+  storageBucket: "fiap-aulas-14d7a.firebasestorage.app",
+  messagingSenderId: "347948116101",
+  appId: "1:347948116101:web:1b7185926185dcdadca1ec",
 };
 
 const app = initializeApp(firebaseConfig);
